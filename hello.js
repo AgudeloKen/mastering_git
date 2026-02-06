c86a361aeab24877c659cf9ea1afff390c365494
Ken <agudelokendev@gmail.com>
2026-02-06 14:55:56 -0500

    Revert "Add console log"
    This reverts commit 8683197427f2bacfe97230c8a93a24e7e2487fd6.

--- hello.js
+++ hello.js
@@ -2,8 +2,3 @@ console.log("Hello world git!");
 console.log("Hello world git!");
 console.log("Hello world git!");
 console.log("Hello world git!");
-
-console.log("TRYING TO REVERT!!!!");
-console.log("TRYING TO REVERT!!!!");
-console.log("TRYING TO REVERT!!!!");
-console.log("TRYING TO REVERT!!!!");
